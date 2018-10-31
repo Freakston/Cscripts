d27a1b0a81d8ce4271d85552973df41d3252f2e6
Freakston <kssuraj15@gmail.com>
2018-10-31 19:04:45 +0530

    Adding a addtion script

---
+++ Addition_calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double num1;
+	double num2;
+	printf("Enter your first number: ");
+	scanf("%lf", &num1);
+	printf("Enter your second number: ");
+	scanf ("%lf", &num2);
+	printf("The answer is: %f\n", num1 + num2);
+}
