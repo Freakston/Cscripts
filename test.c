9e2eacae0101daab2eca8f17591dafb71831d710
Freakston <kssuraj15@gmail.com>
2018-10-31 18:45:24 +0530

    Just testing

--- test.c
+++ test.c
@@ -82,10 +82,10 @@
 // }
 int main()
 {
-	char grade;	
-	printf("Enter your Grade: ");
-	scanf("%c", &grade);
-	printf("Your Grade  is %c\n", grade);
+	char name[20];	
+	printf("Enter your name: ");
+	fgets(name, 20, stdin);
+	printf("Your name is %s\n", name);
 
 	return 0;
 }
