a7a58d8636ef29e9517552ab50485f8734bf94ca
Freakston <kssuraj15@gmail.com>
2018-11-01 18:32:41 +0530

    Added a small UI

--- stack2.c
+++ stack2.c
@@ -18,11 +18,19 @@ void pop()
 }
 
 int main (){
-	for (int b = 0; b < 10; ++b)
+	int ch, l,b;
+	printf("Enter choice 1:Push 2:Pop\n");
+	scanf("%d", &ch);
+	if (ch == 1)
 	{
-	printf("Enter the number you would like to push\n");
-	scanf("%d", &y);
-	push(y);	
+	 printf("How many would you like to push ?\n");	
+	 scanf("%d", &l);
+	 for (b=l ; b >0; --b )
+	 {
+	 	printf("Enter the number you would like to push\n");
+		scanf("%d", &y);
+		push(y);
+		}
 	}
 	printf("How many of them would you like to pop\n");
 	scanf("%d", &limit);
